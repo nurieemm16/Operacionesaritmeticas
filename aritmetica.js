d4ae500fce1e01177110e626d6bd35521a64eb1f
nurieemm16 <nuriamarzo24@gmail.com>
2024-04-10 19:32:57 +0200

    subida operaciones aritmeticas

---
+++ aritmetica.js
@@ -0,0 +1,34 @@
+//EJERCICIO 1 
+
+const numeroDeSaludos = 3;
+// Bucle que se ejecuta x veces
+for(let i = 0; i < numeroDeSaludos; i++){
+    console.log("Hola Marga");
+    console.log("Adiós Carol");
+}
+
+//EJERCICIO 2 
+
+
+//Números introducidos usuario:
+let num1 = 4;
+let num2 = 5;
+let num3 = 200;
+let num4 = 50;
+
+
+function sumar (a, b){
+    return a + b
+}
+console.log(sumar(num1, num2))
+
+
+
+function restar (a, b){
+    return a - b
+}
+
+console.log(restar(num3, num4)
+)
+
+
